53f85646b82b7a75daf5f36042e76f503775209e
etiandre <eti.andre@gmail.com>
2018-11-10 20:12:32 +0000

    works moyen

--- multicam.py
+++ multicam.py
@@ -2,6 +2,7 @@ import pygame
 import pygame.camera
 from pygame.locals import *
 import time
+import subprocess
 
 pygame.init()
 pygame.camera.init()
@@ -9,52 +10,74 @@ pygame.camera.init()
 
 class Capture(object):
     def __init__(self):
-        self.preview_size = (1920, 1080)
+        # self.camera_size = (2304, 1536)
+        self.camera_size = (1920, 1080)
+        self.resolution = (1920, 1080)
         self.img_path = "img/"
-        # self.preview_size = (2304, 1536)
-        # create a display surface. standard pygame stuff
-
-        # this is the same as what we saw before
-        
+        self.start_cams(self.camera_size)
+        # setup display
+        self.display = pygame.display.set_mode(self.resolution, 0)
+        # create a surface to capture to.  for performance purposes
+        # bit depth is the same as that of the display surface.
+        self.snapshots = [pygame.surface.Surface(self.camera_size, 0, self.display)
+                         for i in self.cams]
+        self.countdown = 0
+        # calculate preview size
+        r = pygame.Rect((0,0), self.resolution)
+        c = pygame.Rect((0,0), (self.camera_size[1], self.camera_size[0]))
+        s = c.fit(r)
+        self.preview_size = s.size
+        self.font = pygame.font.Font(pygame.font.get_default_font())
+    def start_cams(self, size):
         #self.clist = pygame.camera.list_cameras()
         self.clist = ["/dev/video3", "/dev/video0", "/dev/video1"]  
         if not self.clist:
             raise ValueError("Sorry, no cameras detected.")
-        self.cams = []
+        
+        self.cams = []  
         for i in self.clist:
             try:
-                cam = pygame.camera.Camera(i, self.preview_size)
+                cam = pygame.camera.Camera(i, self.camera_size)
                 cam.start()
                 self.cams.append(cam)
             except SystemError:
                 print("Error opening {}".format(i))
-        self.display = pygame.display.set_mode((self.preview_size[1]*len(self.cams),self.preview_size[0]), 0)
-        # create a surface to capture to.  for performance purposes
-        # bit depth is the same as that of the display surface.
-        self.snapshots = [pygame.surface.Surface(self.preview_size, 0, self.display)
-                         for i in self.cams]
-    def get_and_flip(self):
+
+    def update_cams(self):
         # if you don't want to tie the framerate to the camera, you can check
         # if the camera has an image ready.  note that while this works
         # on most cameras, some will never return true.
         for i,cam in enumerate(self.cams):
             if cam.query_image():
                 cam.get_image(self.snapshots[i])
-                img = pygame.transform.rotate(self.snapshots[i], 270)
-                img = pygame.transform.flip(img, True, False)
-                self.display.blit(img, (self.preview_size[1]*i, 0))
-
-        # blit it to the display surface.  simple!
-        pygame.display.flip()
+    
+    def draw(self):
+        for i in range(len(self.snapshots)):
+            img = pygame.transform.flip(pygame.transform.rotate(self.snapshots[i], 270), True, False)
+            self.display.blit(pygame.transform.scale(img, self.preview_size), (self.preview_size[0]*i, 0))
+        
     def do_capture(self):
         t = time.strftime("%d%m%y-%H%M%S")
         for i in range(len(self.snapshots)):
-            pygame.image.save(pygame.transform.rotate(self.snapshots[i], 270), self.img_path+"IMG-{}-{}.jpg".format(t, i))
+            pygame.image.save(pygame.transform.rotate(self.snapshots[i], 270), "{}.jpg".format(i))
+            subprocess.call(["cp", "{}.jpg".format(i), self.img_path+"IMG-{}-{}.jpg".format(t, i)])
         print("Picture taken at {}".format(t))
+    
+    def draw_stitch(self):
+        subprocess.call(["nona", "-m", "JPEG", "-o", "out", "params.pto"])
+        self.stitch = pygame.image.load("out.jpg")
+        r = pygame.Rect((0,0), self.resolution)
+        c = pygame.Rect((0,0), self.stitch.get_size())
+        s = c.fit(r)
+        print(s)
+        self.display.blit(pygame.transform.scale(self.stitch, s.size), (0,0))
+        pygame.display.flip()
+        pygame.time.wait(5000)
+
     def main(self):
         going = True
         while going:
-            self.get_and_flip()
+            self.update_cams()
             events = pygame.event.get()
             for e in events:
                 if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
@@ -62,10 +85,27 @@ class Capture(object):
                     for cam in self.cams:
                         cam.stop()
                     going = False
-                if e.type == KEYDOWN and e.key == K_SPACE:
-                    self.do_capture()
-
-
+                elif e.type == KEYDOWN and e.key == K_SPACE and self.countdown == 0:
+                    self.countdown = 3
+                    pygame.time.set_timer(USEREVENT+1, 1000)
+                    print "{} !".format(self.countdown)
+                elif e.type == USEREVENT+1:
+                    if self.countdown > 1:
+                        self.countdown -= 1
+                        text = self.font.render("{}".format(self.countdown), True, (255, 255, 255))
+                        self.display.blit(text, (
+                                self.resolution[0] - text.get_width() // 2,
+                                self.resolution[1] - text.get_height() // 2
+                        ))
+                    else:
+                        self.countdown = 0
+                        pygame.time.set_timer(USEREVENT+1, 0)
+                        self.display.fill(pygame.Color(255,255,255,255))
+                        pygame.display.flip()
+                        self.do_capture()
+                        self.draw_stitch()
+            self.draw()
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
